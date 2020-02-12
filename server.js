a02b35d3edb963701a28e2ab9601226a5e0ecddf
Jan Krejca <jantheprogrammer@gmail.com>
2020-02-12 13:28:45 -0800

    Add of little bit of responsivness

--- server.js
+++ server.js
@@ -11,13 +11,17 @@ const PORT = process.env.PORT || 8000
 const app = express()
 
 mongoose
-  .connect(db, { useNewUrlParser: true, useFindAndModify: false })
+  .connect(db, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err))
 
 //bodyParser middlewear
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 app.use(express.static(path.resolve(__dirname, './client/build')))
@@ -25,11 +29,11 @@ app.use(express.static(path.resolve(__dirname, './client/build')))
 app.use('/api/task', task)
 
 // All remaining requests return the React app, so it can handle routing.
-app.get('*', function (req, res) {
+app.get('*', function(req, res) {
   res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
 })
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   console.error(`Server is listening on port ${PORT}`)
 })
 // }
